5cf98d5a66b8f5da5a050f79a678b743caaf52f4
ankushdubey <ankush.kv@gmail.com>
2020-02-01 15:20:31 +0000

    ME_QBOX_MODULE_LINUX_COMMANDS

--- linux_commands/print_directory_structure.sh
+++ linux_commands/print_directory_structure.sh
@@ -9,10 +9,10 @@
     # 2) If there are multiple commands, either put a semicolon between the commands or write each command on a separate line
     # 3) Execute the script before submission and confirm the output. Delete the created directories/files, if any, before testing the script
 pwd
-mkdir -p /home/crio-user/workspace/QBox/backup
-mkdir -p /home/crio-user/workspace/QBox/transfer
-cp /home/crio-user/workspace/QBox/logs/vsftpd1.log /home/crio-user/workspace/QBox/backup
-cd /home/crio-user/workspace/QBox
+mkdir ~/workspace/QBox/backup
+mkdir ~/workspace/QBox/transfer
+cp ~/workspace/QBox/logs/vsftpd1.log ~/workspace/QBox/backup
+cd ~/workspace/QBox
 tree
 
 
