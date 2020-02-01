0dc71310cc512b21a357014e3009105bbcc1cb8c
ankushdubey <ankush.kv@gmail.com>
2020-02-01 15:24:02 +0000

    ME_QBOX_MODULE_LINUX_COMMANDS

--- linux_commands/print_directory_structure.sh
+++ linux_commands/print_directory_structure.sh
@@ -8,7 +8,7 @@
     #    Run the “tree” command from QBox directory without any parameters.
     # 2) If there are multiple commands, either put a semicolon between the commands or write each command on a separate line
     # 3) Execute the script before submission and confirm the output. Delete the created directories/files, if any, before testing the script
-pwd
+
 mkdir ~/workspace/QBox/backup
 mkdir ~/workspace/QBox/transfer
 cp ~/workspace/QBox/logs/vsftpd1.log ~/workspace/QBox/backup
